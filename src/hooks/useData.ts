b2bcfde45685338a111288990bbea777c9741a13
Ammar Shah <ammarshah123@gmail.com>
2024-06-03 17:50:15 +0500

    Filter games by genre

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { CanceledError } from "axios";
+import { AxiosRequestConfig, CanceledError } from "axios";
 import apiClient from "../services/api-client";
 
 interface ApiResponse<T> {
@@ -7,30 +7,40 @@ interface ApiResponse<T> {
   results: T[];
 }
 
-function useData<T>(endpoint: string) {
+function useData<T>(
+  endpoint: string,
+  requestConfig?: AxiosRequestConfig,
+  dependencies?: any[]
+) {
   const [data, setData] = useState<T[]>([]);
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    const controller = new AbortController();
+  useEffect(
+    () => {
+      const controller = new AbortController();
 
-    setIsLoading(true);
+      setIsLoading(true);
 
-    apiClient
-      .get<ApiResponse<T>>(endpoint, { signal: controller.signal })
-      .then((response) => {
-        setData(response.data.results);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        if (error instanceof CanceledError) return;
-        setError(error.message);
-        setIsLoading(false);
-      });
+      apiClient
+        .get<ApiResponse<T>>(endpoint, {
+          signal: controller.signal,
+          ...requestConfig,
+        })
+        .then((response) => {
+          setData(response.data.results);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          if (error instanceof CanceledError) return;
+          setError(error.message);
+          setIsLoading(false);
+        });
 
-    return () => controller.abort(); // cleanup function
-  }, []);
+      return () => controller.abort(); // cleanup function
+    },
+    dependencies ? [...dependencies] : []
+  );
 
   return { data, error, isLoading };
 }
