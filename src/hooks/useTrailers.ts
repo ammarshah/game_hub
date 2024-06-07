1857dda4cd993251ebe9fbf49009271eed613d7f
Ammar Shah <ammarshah123@gmail.com>
2024-06-07 20:08:17 +0500

    Builds game trailer

---
+++ src/hooks/useTrailers.ts
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import ms from "ms";
+import APIClient from "../services/api-client";
+import { Trailer } from "../entities/Trailer";
+
+function useTrailers(gameId: number) {
+  const apiClient = new APIClient<Trailer>(`/games/${gameId}/movies`);
+
+  return useQuery({
+    queryKey: ["trailers", gameId],
+    queryFn: apiClient.getAll,
+    staleTime: ms("24h"),
+  });
+}
+
+export default useTrailers;
